0a70b23c2b9e8be5147574fea401217291cffd3c
GilsonGangadhar-btr <gilson@btr-india.co.in>
2025-07-09 22:54:58 +0530

    spamming & bot protection

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import cookieParser from "cookie-parser";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 
 const app = express();
 const port = PORT || 3000;
@@ -13,6 +14,7 @@ const port = PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
