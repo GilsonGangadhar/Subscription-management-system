d807b5b50e3124b3ab465573ef6cebdaf876d520
GilsonGangadhar-btr <gilson@btr-india.co.in>
2025-07-10 21:32:22 +0530

    setting up reminder workflows

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import subscriptionRouter from "./routes/subscription.routes.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import cookieParser from "cookie-parser";
 import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
+import workflowRouter from "./routes/workflow.routes.js";
 
 const app = express();
 const port = PORT || 3000;
@@ -18,7 +19,8 @@ app.use(arcjetMiddleware);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
-app.use("api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/workflows", workflowRouter);
 
 app.use(errorMiddleware);
 
